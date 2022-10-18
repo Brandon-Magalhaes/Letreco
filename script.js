c89d19d336787cc882f7288407b09418a18110e5
Brandon Magalhães <brandonmagalhaes.developer@hotmail.com>
2022-10-18 18:45:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ const checkGuess = () => {
     return
 } {
     if(currentRow === rows -1) {
-        window.alert("Errou!")
+        window.alert("You Lose!")
     } else {
         moveToNextRow()
     }
